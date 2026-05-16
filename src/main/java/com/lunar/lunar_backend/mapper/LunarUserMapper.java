5d982d5f28459e3796c7e8e76d610a3a5e76b2dc
Haze <2488292668@qq.com>
2026-05-16 15:43:29 +0800

    :sparkles: feat(mapper): 添加数据访问层映射器接口
    - 在LunarBackendApplication中配置MapperScan注解
    - 创建ChartRecordMapper接口继承BaseMapper
    - 创建LunarUserMapper接口继承BaseMapper
    - 配置MyBatis-Plus的通用数据访问功能
    - 定义图表记录实体的数据操作接口
    - 定义用户实体的数据操作接口

---
+++ src/main/java/com/lunar/lunar_backend/mapper/LunarUserMapper.java
@@ -0,0 +1,7 @@
+package com.lunar.lunar_backend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lunar.lunar_backend.entity.LunarUser;
+
+public interface LunarUserMapper extends BaseMapper<LunarUser> {
+}
